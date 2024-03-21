31ac64c18a187e7b84e5d37816a06af7ca8bddfc
Valentin <145005466+Valesz1022@users.noreply.github.com>
2024-03-21 11:07:51 +0100

    profil adatainak módosításának megírása

--- app/ahoy/index/forms.py
+++ app/ahoy/index/forms.py
@@ -1,1 +1,62 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import PasswordChangeForm as DjangoPasswordChangeForm
+
+class UserProfileForm(forms.ModelForm):
+
+    username = forms.CharField(
+        max_length=150,
+        help_text="",
+        label="Felhasználónév",
+        
+    )
+    first_name = forms.CharField(
+        label="Keresztnév",
+    )
+    last_name = forms.CharField(
+        label="Vezetéknév",
+    )
+    email = forms.EmailField(
+        max_length=254,
+        help_text="",
+        label="Email-cím",
+    )
+
+    class Meta:
+            model = User
+            fields = (
+                "username",
+                "first_name",
+                "last_name",
+                "email",
+            )
+
+
+class CustomPasswordChangeForm(DjangoPasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomPasswordChangeForm, self).__init__(*args, **kwargs)
+        
+        self.fields['new_password1'] = forms.CharField(
+            widget=forms.PasswordInput(),
+            help_text="",
+            label="Új jelszó"
+        )
+        self.fields['new_password2'] = forms.CharField(
+            widget=forms.PasswordInput(),
+            help_text="",
+            label="Új jelszó mégegyszer"
+        )
+
+        self.fields['old_password'] = forms.CharField(
+            widget=forms.PasswordInput(),
+            help_text="",
+            label="Régi jelszó"
+        )
+
+    class Meta:
+        model = User
+        fields = (
+            "old_password",
+            "new_password1",
+            "new_password2",
+        )
--- app/ahoy/index/urls.py
+++ app/ahoy/index/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('profile/', views.profile, name='profile'),
+    path('change_data/', views.change_data, name='change_data'),
+    path('change_password/', views.change_password, name='change_password'),
     path('logout_view/', views.logout_view, name='logout_view'),
 ]
 
