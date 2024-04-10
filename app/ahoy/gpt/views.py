91257e72080cbc46ab04fd28820220caa99ea261
Drntth <tothdi03@gmail.com>
2024-04-10 15:04:12 +0200

    working GPT

--- app/ahoy/gpt/views.py
+++ app/ahoy/gpt/views.py
@@ -1,27 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from freeGPT import AsyncClient
-from asyncio import run
+from freeGPT.Client.gpt4 import Completion
 
 @login_required(redirect_field_name="bejelentkezes_szukseges")
 def gpt(request):
-    message = request.GET.get('message', None) 
-    response = ""
-
     if request.method == 'POST':
-        input_text = request.POST.get('input', '')
-        if input_text:
-            response = generate_gpt_response(input_text)
-
-    if message:
-        return render(request, 'gpt.html', {'message': message, 'response': response})
+        prompt = request.POST.get('input')
+        try:
+            completion_generator = Completion()
+            response = completion_generator.create(prompt)
+            return render(request, 'gpt.html', {'response': response})
+        except Exception as e:
+            error_message = f"Error: {e}"
+            return render(request, 'gpt.html', {'error_message': error_message})
     else:
-        return render(request, 'gpt.html', {'response': response}) 
-
-async def generate_gpt_response(input_text):
-    try:
-        gpt_instance = AsyncClient()
-        response = await gpt_instance.create_completion("MODEL", input_text)
-        return response
-    except Exception as e:
-        return str(e)
+        return render(request, 'gpt.html')
