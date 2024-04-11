68d38139875edba1ba71106b9cdbc89f08e0be76
Fecsk3 <95655472+Fecsk3@users.noreply.github.com>
2024-04-11 11:56:59 +0200

    404 oldal fixálása
    Fixáltam a hibát, ahol a design és a 404 oldal nem fut egyszerre.

--- app/ahoy/ahoy/urls.py
+++ app/ahoy/ahoy/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import RedirectView
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
+    re_path(r'static/(?P<path>.*)', serve, {'document_root': settings.STATIC_ROOT}),
     path('', include('index.urls')),
     path('', include('register.urls')),
     path('', include('login.urls')),
