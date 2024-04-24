8f17b473c5b6477c99f188dd727803452d97e598
Drntth <tothdi03@gmail.com>
2024-04-24 19:47:59 +0200

    edit, delete, create, (colors)

--- app/ahoy/kanban/models.py
+++ app/ahoy/kanban/models.py
@@ -22,6 +22,7 @@ class Card(models.Model):
     description = models.TextField(blank=True)
     column = models.ForeignKey(Column, related_name='cards', on_delete=models.CASCADE)
     order = models.IntegerField(default=0)
+    color = models.CharField(max_length=7, default="#14BDEB", null=True)
 
     def __str__(self):
         return self.title
