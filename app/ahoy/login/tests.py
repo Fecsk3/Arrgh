4764dc5756903ad0020ed77dd6f23a5fd28c9334
Valentin <145005466+Valesz1022@users.noreply.github.com>
2024-05-22 22:01:33 +0200

    login test
    ahogy vártam volt hiba a kóddal, egész pontosan 2 hibát talált a teszt. De egyébként lefutott rendesen a teszt

--- app/ahoy/login/tests.py
+++ app/ahoy/login/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .views import user_login
+from .forms import LoginForm
 
-# Create your tests here.
+class LoginViewTest(TestCase):
+    def setUp(self):
+        self.login_url = reverse('login')
+        self.user = User.objects.create_user(username='test_user', password='Test1234')
+
+    def test_login_page_render(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login.html')
+
+    def test_successful_login(self):
+        response = self.client.post(self.login_url, {'username': 'test_user', 'password': 'Test1234'})
+        self.assertRedirects(response, reverse('index'))
+
+    def test_unsuccessful_login(self):
+        response = self.client.post(self.login_url, {'username': 'test_user', 'password': 'incorrect_password'})
+        self.assertEqual(response.status_code, 302)  # Visszaállítottam a várt válaszkódot 302-re
+        self.assertRedirects(response, reverse('login'))
+
+
+    def test_invalid_form_submission(self):
+        form_data = {'username': '', 'password': ''}  # Hibás form adatok
+        response = self.client.post(self.login_url, form_data)
+        self.assertEqual(response.status_code, 200)  # Ellenőrizd, hogy a válaszkód 200-as
+        self.assertTemplateUsed(response, 'login.html')  # Ellenőrizd, hogy a megfelelő template-t használja
+        form = response.context['form']  # Az űrlap a kontextusban van
+        self.assertTrue(form.errors)  # Ellenőrizzük, hogy vannak hibák a formában
+        self.assertContains(response, 'This field is required.')  # Ellenőrizzük, hogy a válasz tartalmazza-e az elvárt hibaüzenetet
+
+
+
+
+    def test_authenticated_user_redirect(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.login_url)
+        self.assertRedirects(response, reverse('index'))
--- app/ahoy/login/views.py
+++ app/ahoy/login/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 from django.contrib import messages
+from django.urls import reverse
+from django.contrib.messages import constants as messages_constants
 
 def user_login(request):
     if request.method == 'POST':
@@ -12,17 +14,15 @@ def user_login(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('/index')
+                return redirect(reverse('index'))
             else:
-                messages.error(request, 'Hibás felhasználónév vagy jelszó!')
-                return redirect('/login')
+                messages.add_message(request, messages_constants.ERROR, 'Hibás felhasználónév vagy jelszó!')
         else:
-            messages.error(request, 'Hibás adatok!')
-            return redirect('/login')
+            messages.add_message(request, messages_constants.ERROR, 'Hibás adatok vagy felhasználónév!')
     else:
         form = LoginForm()
         if request.user.is_authenticated:
             message = "A bejelentkezés felület nem elérhető bejelentkezett felhasználóknak!"
-            messages.info(request, message)
-            return redirect('/index')
-    return render(request, 'login.html', {'form': form})
+            messages.add_message(request, messages_constants.INFO, message)
+            return redirect(reverse('index'))
+    return render(request, 'login.html', {'form': form, 'messages': messages.get_messages(request)})
